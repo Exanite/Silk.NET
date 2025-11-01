abf7f9c592a4dfc6c3d238fac42676f4a5ff6f09
William Chen <exanite78012@gmail.com>
2025-11-01 19:10:56 -0400

    Use official slang shared library names

--- src/Slang/Silk.NET.Slang/SlangLibraryNameContainer.cs
+++ src/Slang/Silk.NET.Slang/SlangLibraryNameContainer.cs
@@ -11,21 +11,21 @@ namespace Silk.NET.Slang
     internal class SlangLibraryNameContainer : SearchPathContainer
     {
         /// <inheritdoc />
-        public override string[] Linux => new[] { "libshaderslang.so" };
+        public override string[] Linux => new[] { "libslang-compiler.so" };
 
         /// <inheritdoc />
-        public override string[] MacOS => new[] { "libshaderslang.dylib" };
+        public override string[] MacOS => new[] { "libslang-compiler.dylib" };
 
         /// <inheritdoc />
-        public override string[] Android => new[] { "libshaderslang.so" };
+        public override string[] Android => new[] { "libslang-compiler.so" };
 
         /// <inheritdoc />
-        public override string[] IOS => new[] { "libshaderslang.dylib" };
+        public override string[] IOS => new[] { "libslang-compiler.dylib" };
 
         /// <inheritdoc />
-        public override string[] Windows64 => new[] { "shaderslang.dll" };
+        public override string[] Windows64 => new[] { "slang-compiler.dll" };
 
         /// <inheritdoc />
-        public override string[] Windows86 => new[] { "shaderslang.dll" };
+        public override string[] Windows86 => new[] { "slang-compiler.dll" };
     }
 }
